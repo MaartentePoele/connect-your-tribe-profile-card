eb7398bb4f683a10ab20645b753366ffe7be24a4
MaartentePoele <maarten.te.poele@hva.nl>
2026-02-02 12:10:43 +0100

    Eerste commit van mijn profile card met NodeJS, Express en Liquid

---
+++ public/scripts/script.js
@@ -0,0 +1,6 @@
+const button = document.querySelector("button");
+
+button.addEventListener("click", function () {
+    document.querySelector("body").classList.toggle("light");
+    document.querySelector("article").classList.toggle("profile-card-light");
+});
